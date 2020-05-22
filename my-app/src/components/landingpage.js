d96a506e51ead2351631e8070db44d67f153c8e8
Assad Farid <assadfarid8@gmail.com>
2020-05-22 17:41:17 +0100

    Initial commit

---
+++ my-app/src/components/landingpage.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
+
+class Landing extends Component {
+  render() {
+    return (
+      <div style={{ width: "100", margin: "auto" }}>
+        <Grid className="landing-grid">
+          <Cell col={12}>
+            <img
+              src="https://cdn4.iconfinder.com/data/icons/diversity-v2-0-volume-03/64/footballer-soccer-iniesta-barcelona-spain-512.png"
+              alt="avatar"
+              className="avatar-img"
+            />
+            <div className="banner-text">
+              <h1>Full Stack Web Developer</h1>
+
+              <hr />
+
+              <p>
+                HTML/CSS | Java | JavaScript | React.js | React.Native | Swift |
+                NodeJS | MySQL</p>
+
+                <div className="social-links">
+                    {/* LinkedIn Icon */}
+                    <a href="http://Google.com" rel="noopener noreferrer" target=" blank"> 
+                    <i className="fa fa-linkedin-square" aria-hidden="true" />
+                    </a>
+
+                    {/* GitHub Icon */}
+                    <a href="http://Google.com" rel="noopener noreferrer" target=" blank"> 
+                    <i className="fa fa-github-square" aria-hidden="true" />
+                    </a>
+
+                    {/* YouTube Icon */}
+                    <a href="http://Google.com" rel="noopener noreferrer" target=" blank"> 
+                    <i className="fa fa-youtube-square" aria-hidden="true" />
+                    </a>
+
+                    {/* FaceBook Icon */}
+                    <a href="http://Google.com" rel="noopener noreferrer" target=" blank"> 
+                    <i className="fa fa-facebook-square" aria-hidden="true" />
+                    </a>
+
+
+                </div>
+            </div>
+          </Cell>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default Landing;
---
+++ my-app/src/components/projects.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { Tabs, Tab  } from "react-mdl";
+
+
+class Projects extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeTab: 0 };
+  }
+  render() {
+    return (
+      <div className="category-tabs">
+        <Tabs
+          activeTab={this.state.activeTab}
+          onChange={(tabID) => this.setState({ activeTab: tabID })}ripple>
+
+              <Tab>React</Tab>
+              <Tab>Java</Tab>
+              <Tab>NodeJS</Tab>
+              <Tab>MySQL</Tab>
+
+          </Tabs>
+          
+      </div>
+    );
+  }
+}
+
+export default Projects;
